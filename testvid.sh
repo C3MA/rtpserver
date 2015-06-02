eab58e6b4ed949e7ae06305010a9cab17417bd38
C3MADev <vm@C3MADev>
2015-06-02 18:40:08 +0200

    Adapted test script

--- testvid.sh
+++ testvid.sh
@@ -1,4 +1,15 @@
-gst-launch  \
+#!/bin/bash
+
+# Requirement for this script is gstreamer-tools
+
+# 
+if [ ! -f /usr/bin/gst-launch ]; then
+ echo "YOU MUST install the required tool!"
+ sudo aptitude install gstreamer-tools
+fi
+
+# the command itself
+/usr/bin/gst-launch  \
       videotestsrc pattern=red \
    !  video/x-raw-rgb, format=\(fourcc\)RGB, width=12, height=10, frame-rate=1/1 \
    !  rtpvrawpay \
